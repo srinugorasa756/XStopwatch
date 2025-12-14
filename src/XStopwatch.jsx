686de5a9c3c2a4f3a94aa3559e19941ad01e82a5
Srinivas <srinugorasa756@gmail.com>
2025-12-14 19:58:02 +0530

    Add useEffect import to Stopwatch component

--- src/XStopwatch.jsx
+++ src/XStopwatch.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 function Stopwatch() {
   const [time, setTime] = useState(0);
@@ -47,5 +47,6 @@ function Stopwatch() {
   );
 }
 export default Stopwatch;
+
 
 
